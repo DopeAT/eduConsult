3cb545af203670574954a24432c9bdde8eff6958
Alex Tser <alextser@yahoo.co.uk>
2020-04-24 19:03:33 +0100

    Added Users Admin

---
+++ resources/views/admin/settings/index.blade.php
@@ -0,0 +1,28 @@
+@extends('admin.layouts.app')
+@section('title', 'Settings Admin Dashboard | EduConsultOrg')
+@section('heading', 'Settings - Dashboard')
+
+@section('content')
+
+    <!-- Content Row -->
+    <div class="row">
+
+        <div class="col-sm-12 bg-white mb-5">
+            <div class="py-3 px-1 d-flex justify-content-between">
+                <h5 class="font-weight-bold">
+                    <i class="fas fa-cogs"></i> Settings
+                </h5>
+            </div>
+
+            @include('layouts.messages')
+        </div>
+
+        <div class="col-sm-12 bg-white">
+
+
+
+        </div>
+
+    </div>
+
+@endsection
